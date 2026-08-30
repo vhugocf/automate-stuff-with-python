e994e0512fed92e7233646dcb016bdf6a3579d55
vhugocfdev <vhugocf@hotmail.com>
2026-08-30 00:31:46 -0300

    lesson complete

---
+++ chapter01/exercise01.py
@@ -0,0 +1,15 @@
+### PRACTICE PROJECTS
+
+## RETANGULE PRINTER - Write a program that prints a rectangle of capital O characters. 
+## For example, the following rectangle has a width of eight and a height of five:
+
+print("Enter the width for the rectangule:...")
+
+width = input()
+width =int(width)
+
+print("7" * width )
+print("7" * width )
+print("7" * width )
+print("7" * width )
+print("7" * width )
