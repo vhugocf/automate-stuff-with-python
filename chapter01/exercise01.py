f4879131eba96a4079a475ba6d0f2616d121fef1
vhugocfdev <vhugocf@hotmail.com>
2026-08-30 23:27:49 -0300

    more updates

--- chapter01/exercise01.py
+++ chapter01/exercise01.py
@@ -13,3 +13,23 @@ print("7" * width )
 print("7" * width )
 print("7" * width )
 print("7" * width )
+
+### PERIMENTER AND AREA CALCULATOR
+## white a program that accepts the width and length
+
+print("Enter the width for the rectangule...")
+width_retangule = input()
+width_retangule = int(width_retangule)
+
+print("Enter the lenght for the rectangule...")
+lenght_retangule = input()
+lenght_retangule = int(lenght_retangule)
+
+area = width_retangule * lenght_retangule
+
+print("The area of the rectangule is...", area)
+
+
+
+
+
