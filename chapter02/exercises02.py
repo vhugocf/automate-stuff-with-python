ca404d360529318bcb4ad491bab9e8648fc1450a
vhugocfdev <vhugocf@hotmail.com>
2026-09-01 00:55:33 -0300

    End of exercise

--- chapter02/exercises02.py
+++ chapter02/exercises02.py
@@ -1,5 +1,11 @@
-print("Enter C or F to indcate Celsius or Fahrenheit:...")
-
+print("Enter C or F to indicate Celsius or Fahrenheit:...")
 scale = input()
+
 print("Enter the temperature:...")
 degrees = int(input())
+
+
+if scale == "C" and degrees >=16 and degrees <=38 or scale == "F" and degrees >= 60 and degrees <= 120:
+    print("Is safe temp")
+else:
+    print("dangerous")
