9c6c94ae96ed6a8ed83f29e8a9cf25c212a20958
vhugocfdev <vhugocf@hotmail.com>
2026-09-02 00:48:34 -0300

    exercice 3 part A complete

--- chapter03/exercises03.py
+++ chapter03/exercises03.py
@@ -1,2 +1,17 @@
-round(42.5)
-print(round(42.5))
+print("Enter the tree size: ")
+
+size = int(input())
+
+for row_num in range(1, size + 1):
+    spaces = " " * (size - row_num)
+    tree = "^" * (row_num * 2 - 1)
+    print(spaces + tree)
+
+spaces = " " * (size - 1)
+print(spaces + "#")
+print(spaces + "#")
+
+
+
+
+
